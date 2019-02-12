be7d43801859654b1859bfcc8bf42ddd818833b7
Miquel Barnada Tinto <milkbarna@gmail.com>
2019-02-12 20:08:22 +0100

    Objects Fall Scripts

---
+++ MarieKondo/Assets/Scripts/FallingObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour {
+
+    [HideInInspector]
+    public bool sparksJoy = false;
+
+    public SpriteRenderer faceImg;
+
+    public float speed = 0.01f;
+
+    int expresion;
+
+    // Use this for initialization
+    void Start()
+    {
+        expresion = Random.Range(0, 7);
+
+        if (expresion < 3)
+            sparksJoy = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - speed, this.transform.position.y);
+    }
+
+    public void getHit(bool sparkBullet)
+    {
+        if (expresion == 6)
+        {
+            expresion = Random.Range(0, 7);
+
+            if (expresion < 3)
+                sparksJoy = true;
+        }
+        else
+        {
+            if (sparkBullet == sparksJoy)
+            {
+                //++ points
+            }
+            else
+            {
+                //-- points
+            }
+            Destroy(this);
+        }
+    }
+
+    public void OnDestroy()
+    {
+        //spawn preaty FXs and shit (diferent destruction depending on how?)
+    }
+
+}
---
+++ MarieKondo/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject objectPrefab;
+
+    public Vector2 timerMinMax = new Vector2(0.5f, 1.6f);
+    public Vector2 posMinMax;
+    float timer = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(posMinMax.x, posMinMax.y), this.transform.position.y);
+            Instantiate(objectPrefab, randomPos, Quaternion.identity);
+            randomTimer();
+        }
+    }
+
+    void randomTimer()
+    {
+        timer = Random.Range(timerMinMax.x, timerMinMax.y);
+    }
+
+}
